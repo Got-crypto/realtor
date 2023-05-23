e0941d21a4f38ab0f40f796d4df8137327dd70f6
Got-crypto <pecpalmplay@gmail.com>
2023-05-24 00:17:01 +0200

    creating search page; adding filters feature

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCancel } from 'react-icons/md'
+
+export default function SearchFilters() {
+    return "sup"
+}
